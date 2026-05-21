1ee4c779fd486be08e66fe0b8ee57d0d19ebaf4b
GhostGamer SCH <antomaxsch@gmail.com>
2026-05-21 15:47:42 +0300

    Update base page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,22 +1,12 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from selenium.common.exceptions import (
-    TimeoutException,
-    ElementClickInterceptedException
-)
-
-from selenium.webdriver.common.action_chains import (
-    ActionChains
-)
-
 
 class BasePage:
 
     def __init__(self, driver):
-
         self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
+        self.wait = WebDriverWait(driver, 20)
 
     def open(self, url):
 
@@ -25,36 +15,15 @@ class BasePage:
     def click(self, locator):
 
         element = self.wait.until(
-            EC.presence_of_element_located(locator)
+            EC.element_to_be_clickable(locator)
         )
 
         self.driver.execute_script(
-            "arguments[0].scrollIntoView({block: 'center'});",
+            "arguments[0].scrollIntoView(true);",
             element
         )
 
-        self.wait.until(
-            EC.visibility_of(element)
-        )
-
-        try:
-
-            self.wait.until(
-                EC.element_to_be_clickable(locator)
-            )
-
-            element.click()
-
-        except (
-                TimeoutException,
-                ElementClickInterceptedException
-        ):
-
-            ActionChains(self.driver) \
-                .move_to_element(element) \
-                .pause(0.5) \
-                .click() \
-                .perform()
+        element.click()
 
     def type(self, locator, text):
 
@@ -64,6 +33,7 @@ class BasePage:
 
         element.clear()
         element.send_keys(text)
+
 
     def get_text(self, locator):
 
